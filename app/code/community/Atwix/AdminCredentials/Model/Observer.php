589f94239b87c5d035d3410d673770d4d770ce4e
enarc <enarc@atwix.com>
2014-01-29 13:24:16 +0200

    Initial commit

---
+++ app/code/community/Atwix/AdminCredentials/Model/Observer.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Atwix
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+
+ * @category    Atwix Mod
+ * @package     Atwix_AdminCredentials
+ * @author      Atwix Core Team
+ * @copyright   Copyright (c) 2014 Atwix (http://www.atwix.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+class Atwix_AdminCredentials_Model_Observer
+{
+    public function enableLoginAutocomplete(Varien_Event_Observer $observer)
+    {
+        $response = $observer->getControllerAction()->getResponse();
+        $body = $response->getBody();
+        $script = '
+            <script type="text/javascript">
+            //<![CDATA[
+                document.observe("dom:loaded", function() {
+                    $("loginForm").setAttribute("autocomplete", "on");
+                    $("dummy").remove();
+                });
+            //]]>
+            </script>';
+
+        $body = str_replace('</body>', $script . '</body>', $body);
+        $response->setBody($body);
+    }
+}
