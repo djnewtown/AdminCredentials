2053eb86a916ee46ff0a0ce25b174efd2dfc64c0
Ingo Fabbri <if@newtown.at>
2016-09-21 15:20:01 +0200

    set login form action
    it is empty by default and results in a 404 if using a custom admin path

--- app/code/community/Atwix/AdminCredentials/Model/Observer.php
+++ app/code/community/Atwix/AdminCredentials/Model/Observer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Atwix
  *
@@ -11,16 +12,23 @@
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
  * to license@magentocommerce.com so we can send you a copy immediately.
-
  * @category    Atwix Mod
  * @package     Atwix_AdminCredentials
+ *
  * @author      Atwix Core Team
  * @copyright   Copyright (c) 2014 Atwix (http://www.atwix.com)
+ *
+ * @author      Ingo Fabbri <if@newtown.at>
+ * @copyright   Copyright (c) 2016 Newtown-Web OG (http://www.newtown.at)
+ *
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-
 class Atwix_AdminCredentials_Model_Observer
 {
+    /**
+     * @event controller_action_postdispatch_adminhtml_index_login
+     * @param Varien_Event_Observer $observer
+     */
     public function enableLoginAutocomplete(Varien_Event_Observer $observer)
     {
         $response = $observer->getControllerAction()->getResponse();
@@ -30,6 +38,7 @@ class Atwix_AdminCredentials_Model_Observer
             //<![CDATA[
                 document.observe("dom:loaded", function() {
                     $("loginForm").setAttribute("autocomplete", "on");
+                    $("loginForm").writeAttribute("action", "' . $this->getUrl('adminhtml') . '");
                     $("dummy").remove();
                 });
             //]]>
